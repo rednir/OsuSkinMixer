a19554978830c2d8785a342e352ba01a1021dfc4
Davran Dilshat <davran.dilshat@gmail.com>
2021-11-09 19:54:20 +0000

    bump version number

--- src/Statics/Settings.cs
+++ src/Statics/Settings.cs
@@ -10,7 +10,7 @@ namespace OsuSkinMixer
 {
     public static class Settings
     {
-        public const string VERSION = "v1.5";
+        public const string VERSION = "v1.6";
 
         public static readonly string AppdataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/osu-skin-mixer";
 
