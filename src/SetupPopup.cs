34374b99a68bf8ce8b95531c849a46e0c571f833
Davran Dilshat <davran.dilshat@gmail.com>
2023-02-17 21:55:12 +0000

    make setup popup functional

--- src/SetupPopup.cs
+++ src/SetupPopup.cs
@@ -1,19 +1,34 @@
 using Godot;
 using System;
+using Environment = System.Environment;
 
 namespace OsuSkinMixer;
 
 public partial class SetupPopup : Control
 {
 	private AnimationPlayer AnimationPlayer;
+	private LineEdit LineEdit;
+	private Button DoneButton;
 
 	public override void _Ready()
 	{
 		AnimationPlayer = GetNode<AnimationPlayer>("Popup/AnimationPlayer");
+		LineEdit = GetNode<LineEdit>("Popup/ScrollContainer/VBoxContainer/ContentPanelContainer/VBoxContainer/LineEdit");
+		DoneButton = GetNode<Button>("Popup/ScrollContainer/VBoxContainer/ContentPanelContainer/VBoxContainer/DoneButton");
+
+		DoneButton.Pressed += DoneButtonPressed;
 	}
 
 	public void In()
 	{
+		LineEdit.Text = Settings.Content.SkinsFolder ?? Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/osu!/Skins";
 		AnimationPlayer.Play("in");
+	}
+
+	private void DoneButtonPressed()
+	{
+		Settings.Content.SkinsFolder = LineEdit.Text;
+		Settings.Save();
+		AnimationPlayer.Play("out");
 	}
 }
