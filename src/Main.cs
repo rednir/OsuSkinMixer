9d7da6898f3293f50ed3580d84ef93d2fb749d9f
Davran Dilshat <davran.dilshat@gmail.com>
2023-02-17 13:48:16 +0000

    pushing scene logic

--- src/Main.cs
+++ src/Main.cs
@@ -8,27 +8,43 @@ public partial class Main : Control
 {
 	private PackedScene MenuScene;
 
+	private AnimationPlayer ScenesAnimationPlayer;
 	private Control ScenesControl;
 
-	private readonly Stack<StackScene> SceneStack = new();
+	private Stack<StackScene> SceneStack { get; } = new();
+
+	private StackScene PendingScene { get; set; }
 
 	public override void _Ready()
 	{
 		MenuScene = GD.Load<PackedScene>("res://src/Menu.tscn");
 
+		ScenesAnimationPlayer = GetNode<AnimationPlayer>("ScenesAnimationPlayer");
 		ScenesControl = GetNode<Control>("Scenes");
+
+		ScenesAnimationPlayer.AnimationFinished += (animationName) =>
+		{
+			if (animationName != "scene_out" || PendingScene == null)
+				return;
+
+			if (SceneStack.TryPeek(out StackScene currentlyActiveScene))
+				currentlyActiveScene.Visible = false;
+
+			PendingScene.ScenePushed += PushScene;
+			PendingScene.Visible = true;
+			SceneStack.Push(PendingScene);
+			ScenesControl.AddChild(PendingScene);
+
+			PendingScene = null;
+			ScenesAnimationPlayer.Play("scene_in");
+		};
 
 		PushScene(MenuScene.Instantiate<StackScene>());
 	}
 
-	public override void _Process(double delta)
-	{
-	}
-
 	private void PushScene(StackScene scene)
 	{
-		scene.ScenePushed += PushScene;
-		SceneStack.Push(scene);
-		ScenesControl.AddChild(scene);
+		PendingScene = scene;
+		ScenesAnimationPlayer.Play("scene_out");
 	}
 }
