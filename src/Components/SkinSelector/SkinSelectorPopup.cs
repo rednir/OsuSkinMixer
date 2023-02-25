92bc1fe39244931e75bddd686f277d0e4848c213
Davran Dilshat <davran.dilshat@gmail.com>
2023-02-25 12:31:27 +0000

    add search bar

--- src/Components/SkinSelector/SkinSelectorPopup.cs
+++ src/Components/SkinSelector/SkinSelectorPopup.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using Skin = OsuSkinMixer.Models.Osu.Skin;
 using OsuSkinMixer.Statics;
+using System.Linq;
 
 namespace OsuSkinMixer.Components.SkinSelector;
 
@@ -10,6 +11,7 @@ public partial class SkinSelectorPopup : Control
 	private PackedScene SkinComponentScene;
 
 	private AnimationPlayer AnimationPlayer;
+	private LineEdit SearchLineEdit;
 	private VBoxContainer SkinsContainer;
 
 	public override void _Ready()
@@ -17,7 +19,10 @@ public partial class SkinSelectorPopup : Control
 		SkinComponentScene = GD.Load<PackedScene>("res://src/Components/SkinSelector/SkinComponent.tscn");
 
 		AnimationPlayer = GetNode<AnimationPlayer>("Popup/AnimationPlayer");
-		SkinsContainer = GetNode<VBoxContainer>("Popup/CanvasLayer/ScrollContainer/VBoxContainer/ContentPanelContainer/SkinComponentsContainer");
+		SkinsContainer = GetNode<VBoxContainer>("%SkinComponentsContainer");
+		SearchLineEdit = GetNode<LineEdit>("%SearchLineEdit");
+
+		SearchLineEdit.TextChanged += OnSearchTextChanged;
 	}
 
 	public void CreateSkinComponents(Action<Skin> onSelected)
@@ -29,6 +34,7 @@ public partial class SkinSelectorPopup : Control
 		{
 			SkinComponent instance = SkinComponentScene.Instantiate<SkinComponent>();
 			SkinsContainer.AddChild(instance);
+			instance.Name = skin.Name;
 			instance.SetValues(skin);
 			instance.Button.Pressed += () =>
 			{
@@ -41,5 +47,11 @@ public partial class SkinSelectorPopup : Control
 	public void In()
 	{
 		AnimationPlayer.Play("in");
+	}
+
+	private void OnSearchTextChanged(string text)
+	{
+		foreach (var component in SkinsContainer.GetChildren().Cast<SkinComponent>())
+			component.Visible = component.Name.ToString().Contains(text, StringComparison.OrdinalIgnoreCase);
 	}
 }
