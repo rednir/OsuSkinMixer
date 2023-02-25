4d3732ff79c619a38accfe73746bdedc895c1da0
Davran Dilshat <davran.dilshat@gmail.com>
2023-02-25 13:27:31 +0000

    add skin modifier scene

--- src/StackScenes/Menu.cs
+++ src/StackScenes/Menu.cs
@@ -9,19 +9,20 @@ public partial class Menu : StackScene
 	public override string Title => "Menu";
 
 	private PackedScene SkinMixerScene;
+	private PackedScene SkinModifierScene;
 
 	private Button SkinMixerButton;
+	private Button SkinModifierButton;
 
 	public override void _Ready()
 	{
 		SkinMixerScene = GD.Load<PackedScene>("res://src/StackScenes/SkinMixer.tscn");
+		SkinModifierScene = GD.Load<PackedScene>("res://src/StackScenes/SkinModifier.tscn");
 
 		SkinMixerButton = GetNode<Button>("%SkinMixerButton");
-		SkinMixerButton.Connect("pressed", new Callable(this, nameof(SkinMixerButtonPressed)));
-	}
+		SkinModifierButton = GetNode<Button>("%SkinModifierButton");
 
-	private void SkinMixerButtonPressed()
-	{
-		EmitSignal(SignalName.ScenePushed, SkinMixerScene.Instantiate<StackScene>());
+		SkinMixerButton.Pressed += () => EmitSignal(SignalName.ScenePushed, SkinMixerScene.Instantiate<StackScene>());
+		SkinModifierButton.Pressed += () => EmitSignal(SignalName.ScenePushed, SkinModifierScene.Instantiate<StackScene>());
 	}
 }
---
+++ src/StackScenes/SkinModifier.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+namespace OsuSkinMixer.StackScenes;
+
+public partial class SkinModifier : StackScene
+{
+	public override string Title => "Skin Modifier";
+
+	public override void _Ready()
+	{
+	}
+}
