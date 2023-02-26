8a3fb7a13eb657045bed58e9ab774c4dc14b1a94
Davran Dilshat <davran.dilshat@gmail.com>
2023-02-26 21:07:13 +0000

    add open logs button + change user dir path

--- src/Components/SettingsPopup.cs
+++ src/Components/SettingsPopup.cs
@@ -7,6 +7,7 @@ public partial class SettingsPopup : Popup
 {
 	private Button UpdateButton;
 	private Button ChangeSkinsFolderButton;
+	private Button OpenLogsButton;
 	private SetupPopup SetupPopup;
 
 	public override void _Ready()
@@ -15,10 +16,12 @@ public partial class SettingsPopup : Popup
 
 		UpdateButton = GetNode<Button>("%UpdateButton");
 		ChangeSkinsFolderButton = GetNode<Button>("%ChangeSkinsFolderButton");
+		OpenLogsButton = GetNode<Button>("%OpenLogsButton");
 		SetupPopup = GetNode<SetupPopup>("%SetupPopup");
 
 		UpdateButton.Pressed += () => OS.ShellOpen($"https://github.com/{Settings.GITHUB_REPO_PATH}/releases/latest");
 		ChangeSkinsFolderButton.Pressed += SetupPopup.In;
+		OpenLogsButton.Pressed += () => OS.ShellOpen(ProjectSettings.GlobalizePath("user://logs"));
 	}
 
 	public void ShowUpdateButton()
