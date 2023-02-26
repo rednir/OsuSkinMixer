0317c09f8a8236566a1315c0d1c4323662b34f30
Davran Dilshat <davran.dilshat@gmail.com>
2023-02-26 16:33:21 +0000

    add change skins button

--- src/Components/SettingsPopup.cs
+++ src/Components/SettingsPopup.cs
@@ -5,8 +5,18 @@ namespace OsuSkinMixer.Components;
 
 public partial class SettingsPopup : Popup
 {
+	private Button ChangeSkinsFolderButton;
+	private SetupPopup SetupPopup;
+
 	public override void _Ready()
 	{
 		base._Ready();
+
+		ChangeSkinsFolderButton = GetNode<Button>("%ChangeSkinsFolderButton");
+		SetupPopup = GetNode<SetupPopup>("%SetupPopup");
+
+		ChangeSkinsFolderButton.Pressed += SetupPopup.In;
 	}
+
+
 }
