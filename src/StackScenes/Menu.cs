a797b3c91491b9fb1859da5108b48a008881527f
Davran Dilshat <davran.dilshat@gmail.com>
2023-02-26 14:28:33 +0000

    basic skin manager scene

--- src/StackScenes/Menu.cs
+++ src/StackScenes/Menu.cs
@@ -9,19 +9,24 @@ public partial class Menu : StackScene
 
 	private PackedScene SkinMixerScene;
 	private PackedScene SkinModifierSkinSelectScene;
+	private PackedScene SkinManagerScene;
 
 	private Button SkinMixerButton;
 	private Button SkinModifierButton;
+	private Button SkinManagerButton;
 
 	public override void _Ready()
 	{
 		SkinMixerScene = GD.Load<PackedScene>("res://src/StackScenes/SkinMixer.tscn");
 		SkinModifierSkinSelectScene = GD.Load<PackedScene>("res://src/StackScenes/SkinModifierSkinSelect.tscn");
+		SkinManagerScene = GD.Load<PackedScene>("res://src/StackScenes/SkinManager.tscn");
 
 		SkinMixerButton = GetNode<Button>("%SkinMixerButton");
 		SkinModifierButton = GetNode<Button>("%SkinModifierButton");
+		SkinManagerButton = GetNode<Button>("%SkinManagerButton");
 
 		SkinMixerButton.Pressed += () => EmitSignal(SignalName.ScenePushed, SkinMixerScene.Instantiate<StackScene>());
 		SkinModifierButton.Pressed += () => EmitSignal(SignalName.ScenePushed, SkinModifierSkinSelectScene.Instantiate<StackScene>());
+		SkinManagerButton.Pressed += () => EmitSignal(SignalName.ScenePushed, SkinManagerScene.Instantiate<StackScene>());
 	}
 }
