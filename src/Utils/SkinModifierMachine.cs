19b540a7d0bf613aedddb2a9c050fd569bd6d3f8
Davran Dilshat <davran.dilshat@gmail.com>
2023-03-07 17:47:53 +0000

    add instafade hitcircle logic

--- src/Utils/SkinModifierMachine.cs
+++ src/Utils/SkinModifierMachine.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Advanced;
 using OsuSkinMixer.Models;
 using OsuSkinMixer.Statics;
 
@@ -74,6 +76,11 @@ public class SkinModifierMachine : SkinMachine
             });
         }
 
+        if (Instafade)
+        {
+            AddTask(() => MakeCirclesInstafade(workingSkin));
+        }
+
         string skinIniDestination = $"{workingSkin.Directory.FullName}/skin.ini";
         AddTask(() =>
         {
@@ -94,6 +101,84 @@ public class SkinModifierMachine : SkinMachine
 
         image.Mutate(i => i.Resize(width, height));
         image.Save(cursorTrailPath);
+    }
+
+    private static void MakeCirclesInstafade(OsuSkin workingSkin)
+    {
+        // With help from https://skinship.xyz/guides/insta_fade_hc.html
+        Settings.Log($"Making circles instafade for skin '{workingSkin.Name}'");
+
+        string skinDirectory = workingSkin.Directory.FullName;
+        string hitcirclePath = $"{skinDirectory}/hitcircle.png";
+        string hitcircleoverlayPath = $"{skinDirectory}/hitcircleoverlay.png";
+
+        Image<Rgba32> hitcircle;
+        Image hitcircleoverlay;
+
+        try
+        {
+            hitcircle = Image.Load<Rgba32>(hitcirclePath);
+            hitcircleoverlay = Image.Load(hitcircleoverlayPath);
+        }
+        catch
+        {
+            // TOOD: use default skin.
+            throw;
+        }
+
+        hitcircleoverlay.Mutate(i => i.Resize((int)(hitcircleoverlay.Width * 1.25), (int)(hitcircleoverlay.Height * 1.25)));
+        hitcircle.Mutate(i => i.Resize((int)(hitcircle.Width * 1.1), (int)(hitcircle.Height * 1.1)));
+
+        Godot.Color[] comboColors = workingSkin.ComboColors;
+
+        for (int i = 0; i <= 9; i++)
+        {
+            string defaultXPath = $"{skinDirectory}/default-{i}.png";
+            Image defaultX = Image.Load(defaultXPath);
+
+            Image<Rgba32> newDefaultX = hitcircle.Clone();
+
+            if (i <= comboColors.Length)
+            {
+                // Modulate the image with the combo color.
+                Godot.Color color = comboColors[(i + 1) % comboColors.Length];
+                for (int x = 0; x < newDefaultX.Width; x++)
+                {
+                    for (int y = 0; y < newDefaultX.Height; y++)
+                    {
+                        Rgba32 oldColor = newDefaultX[x, y];
+                        byte red = (byte)(256.0f * (oldColor.R / 256.0f) * color.R);
+                        byte green = (byte)(256.0f * (oldColor.G / 256.0f) * color.G);
+                        byte blue = (byte)(256.0f * (oldColor.B / 256.0f) * color.B);
+
+                        newDefaultX[x, y] = Color.FromRgba(red, green, blue, oldColor.A);
+                    }
+                }
+            }
+
+            newDefaultX.Mutate(img =>
+            {
+                img.DrawImage(hitcircleoverlay, new Point(
+                    x: (int)((hitcircle.Width * 0.5) - (hitcircleoverlay.Width * 0.5)),
+                    y: (int)((hitcircle.Height * 0.5) - (hitcircleoverlay.Height * 0.5))
+                ), 1);
+                img.DrawImage(defaultX, new Point(
+                    x: (int)((hitcircle.Width * 0.5) - (defaultX.Width * 0.5)),
+                    y: (int)((hitcircle.Height * 0.5) - (defaultX.Height * 0.5))
+                ), 1);
+            });
+
+            if (i == 0)
+                newDefaultX.Mutate(i => i.Opacity(0));
+
+            newDefaultX.Save(defaultXPath);
+        }
+
+        hitcircle.Mutate(i => i.Opacity(0));
+        hitcircleoverlay.Mutate(i => i.Opacity(0));
+
+        hitcircle.Save(hitcirclePath);
+        hitcircleoverlay.Save(hitcircleoverlayPath);
     }
 
     protected override void CopyIniPropertyOption(OsuSkin workingSkin, SkinIniPropertyOption iniPropertyOption)
