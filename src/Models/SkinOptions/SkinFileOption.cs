3adb2793e1e75fd8f3b207dc5f0a3bd1057bfd6c
Davran Dilshat <davran.dilshat@gmail.com>
2023-03-14 14:31:58 +0000

    add friendly display name for some file options

--- src/Models/SkinOptions/SkinFileOption.cs
+++ src/Models/SkinOptions/SkinFileOption.cs
@@ -3,13 +3,16 @@ namespace OsuSkinMixer.Models;
 /// <summary>Represents an option with the target of an image or audio file, or several.</summary>
 public class SkinFileOption : SkinOption
 {
-    public SkinFileOption(string fileName, bool isAudio)
+    public SkinFileOption(string fileName, bool isAudio, string displayName = null)
     {
+        DisplayName = displayName;
         IncludeFileName = fileName;
         IsAudio = isAudio;
     }
 
-    public override string Name => IncludeFileName + (IsAudio ? ".wav" : ".png");
+    public override string Name => DisplayName ?? IncludeFileName + (IsAudio ? ".wav" : ".png");
+
+    public string DisplayName { get; set; }
 
     public string IncludeFileName { get; set; }
 
