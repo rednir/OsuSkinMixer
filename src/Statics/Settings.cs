c4e260c5df826b7add8fd66ba1726e965beea18b
Davran Dilshat <davran.dilshat@gmail.com>
2023-04-05 16:29:09 +0100

    bump version number

--- src/Statics/Settings.cs
+++ src/Statics/Settings.cs
@@ -13,7 +13,7 @@ namespace OsuSkinMixer.Statics;
 
 public static partial class Settings
 {
-    public const string VERSION = "v2.6.0";
+    public const string VERSION = "v2.6.1";
 
     public const string GITHUB_REPO_PATH = "rednir/OsuSkinMixer";
 
