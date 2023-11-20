00d2dbe1de5912ed0f0ebc10576d32aeb22dd9ea
Davran Dilshat <davran.dilshat@gmail.com>
2023-11-20 09:07:47 +0000

    support multiple skin combo colours

--- src/StackScenes/SkinModifierModificationSelect.cs
+++ src/StackScenes/SkinModifierModificationSelect.cs
@@ -10,16 +10,19 @@ public partial class SkinModifierModificationSelect : StackScene
     public override string Title => SkinsToModify.Count == 1 ? $"Modifying: {SkinsToModify[0].Name}" : $"Modifying {SkinsToModify.Count} skins";
 
     private PackedScene SkinInfoScene;
+    private PackedScene ComboColourContainerScene;
 
     private CancellationTokenSource CancellationTokenSource;
 
     public List<OsuSkin> SkinsToModify { get; set; }
 
+    private ComboColoursContainer[] ComboColoursContainers;
+
     private SkinOptionsSelector SkinOptionsSelector;
     private SkinComponent DefaultSkinComponent;
     private SkinComponent BlankComponent;
     private Button ApplyChangesButton;
-    private ComboColoursContainer ComboColoursContainer;
+    private VBoxContainer ComboColoursContainerCollection;
     private CheckBox SmoothTrailCheckBox;
     private CheckBox InstafadeCheckBox;
     private CheckBox DisableAnimationsCheckBox;
@@ -28,12 +31,13 @@ public partial class SkinModifierModificationSelect : StackScene
     public override void _Ready()
     {
         SkinInfoScene = GD.Load<PackedScene>("res://src/StackScenes/SkinInfo.tscn");
+        ComboColourContainerScene = GD.Load<PackedScene>("res://src/Components/Osu/ComboColoursContainer.tscn");
 
         SkinOptionsSelector = GetNode<SkinOptionsSelector>("%SkinOptionsSelector");
         DefaultSkinComponent = GetNode<SkinComponent>("%DefaultSkinComponent");
         BlankComponent = GetNode<SkinComponent>("%BlankComponent");
         ApplyChangesButton = GetNode<Button>("%ApplyChangesButton");
-        ComboColoursContainer = GetNode<ComboColoursContainer>("%ComboColoursContainer");
+        ComboColoursContainerCollection = GetNode<VBoxContainer>("%ComboColoursContainerCollection");
         SmoothTrailCheckBox = GetNode<CheckBox>("%SmoothTrailCheckBox");
         InstafadeCheckBox = GetNode<CheckBox>("%InstafadeCheckBox");
         DisableAnimationsCheckBox = GetNode<CheckBox>("%DisableAnimationsCheckBox");
@@ -43,16 +47,32 @@ public partial class SkinModifierModificationSelect : StackScene
         DefaultSkinComponent.LeftClicked += () => SkinOptionsSelector.OptionComponentSelected(new SkinOptionValue(SkinOptionValueType.DefaultSkin));
         BlankComponent.LeftClicked += () => SkinOptionsSelector.OptionComponentSelected(new SkinOptionValue(SkinOptionValueType.Blank));
         ApplyChangesButton.Pressed += OnApplyChangesButtonPressed;
-        ComboColoursContainer.Skin = SkinsToModify[0];
         LoadingPopup.CancelAction = OnCancelButtonPressed;
         LoadingPopup.DisableCancelAt = SkinModifierMachine.UNCANCELLABLE_AFTER;
 
         OsuData.SkinRemoved += OnSkinRemoved;
+
+        InitialiseComboColourContainers();
     }
 
     public override void _ExitTree()
     {
         OsuData.SkinRemoved -= OnSkinRemoved;
+    }
+
+    private void InitialiseComboColourContainers()
+    {
+        List<ComboColoursContainer> containers = new();
+
+        foreach (var skin in SkinsToModify)
+        {
+            var container = ComboColourContainerScene.Instantiate<ComboColoursContainer>();
+            container.Skin = skin;
+            ComboColoursContainerCollection.AddChild(container);
+            containers.Add(container);
+        }
+
+        ComboColoursContainers = containers.ToArray();
     }
 
     private void OnSkinRemoved(OsuSkin skin)
