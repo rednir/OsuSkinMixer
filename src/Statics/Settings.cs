5af3645c175ef25e2dd50fd7522798dc5af189a2
Davran Dilshat <davran.dilshat@gmail.com>
2026-09-17 18:01:25 +0100

    bump version number

--- src/Statics/Settings.cs
+++ src/Statics/Settings.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 public static partial class Settings
 {
-    public const string VERSION = "v3.3-lazer.1";
+    public const string VERSION = "v3.3-lazer.2";
 
     public const string GITHUB_REPO_PATH = "rednir/OsuSkinMixer";
 
